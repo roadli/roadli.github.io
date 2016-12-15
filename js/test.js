765baec7c26f56a31b39dbb5f91f2e0432a4ff76
roadli <roadli@gmail.com>
2016-12-15 09:51:58 +0800

    Daily update images file @2016-12-15

--- js/test.js
+++ js/test.js
@@ -4,322 +4,322 @@ $(function () {
   blueimp.Gallery([
 {
 href: 'http://7xoqys.com1.z0.glb.clouddn.com/tumblr_oi5zi6yPpZ1uygx22o1_500.jpg',
-type: '',
+type: 'image/jpeg',
 thumbnail: 'http://7xoqys.com1.z0.glb.clouddn.com/tumblr_oi5zi6yPpZ1uygx22o1_500.jpg?imageView2/2/w/200'
 },
 {
 href: 'http://7xoqys.com1.z0.glb.clouddn.com/tumblr_oi5zi6dDu81uygx22o1_500.jpg',
-type: '',
+type: 'image/jpeg',
 thumbnail: 'http://7xoqys.com1.z0.glb.clouddn.com/tumblr_oi5zi6dDu81uygx22o1_500.jpg?imageView2/2/w/200'
 },
 {
 href: 'http://7xoqys.com1.z0.glb.clouddn.com/tumblr_oi60w7hxHO1uygx22o1_500.jpg',
-type: '',
+type: 'image/jpeg',
 thumbnail: 'http://7xoqys.com1.z0.glb.clouddn.com/tumblr_oi60w7hxHO1uygx22o1_500.jpg?imageView2/2/w/200'
 },
 {
 href: 'http://7xoqys.com1.z0.glb.clouddn.com/tumblr_oi60w7lvvO1uygx22o1_500.jpg',
-type: '',
+type: 'image/jpeg',
 thumbnail: 'http://7xoqys.com1.z0.glb.clouddn.com/tumblr_oi60w7lvvO1uygx22o1_500.jpg?imageView2/2/w/200'
 },
 {
 href: 'http://7xoqys.com1.z0.glb.clouddn.com/tumblr_oi62a6kjfz1uygx22o1_500.jpg',
-type: '',
+type: 'image/jpeg',
 thumbnail: 'http://7xoqys.com1.z0.glb.clouddn.com/tumblr_oi62a6kjfz1uygx22o1_500.jpg?imageView2/2/w/200'
 },
 {
 href: 'http://7xoqys.com1.z0.glb.clouddn.com/tumblr_oi62a6eQjt1uygx22o1_500.jpg',
-type: '',
+type: 'image/jpeg',
 thumbnail: 'http://7xoqys.com1.z0.glb.clouddn.com/tumblr_oi62a6eQjt1uygx22o1_500.jpg?imageView2/2/w/200'
 },
 {
 href: 'http://7xoqys.com1.z0.glb.clouddn.com/tumblr_oi63o7r4FI1uygx22o1_500.jpg',
-type: '',
+type: 'image/jpeg',
 thumbnail: 'http://7xoqys.com1.z0.glb.clouddn.com/tumblr_oi63o7r4FI1uygx22o1_500.jpg?imageView2/2/w/200'
 },
 {
 href: 'http://7xoqys.com1.z0.glb.clouddn.com/tumblr_oi63o73eto1uygx22o1_500.jpg',
-type: '',
+type: 'image/jpeg',
 thumbnail: 'http://7xoqys.com1.z0.glb.clouddn.com/tumblr_oi63o73eto1uygx22o1_500.jpg?imageView2/2/w/200'
 },
 {
 href: 'http://7xoqys.com1.z0.glb.clouddn.com/tumblr_oi6526nzLK1uygx22o1_500.jpg',
-type: '',
+type: 'image/jpeg',
 thumbnail: 'http://7xoqys.com1.z0.glb.clouddn.com/tumblr_oi6526nzLK1uygx22o1_500.jpg?imageView2/2/w/200'
 },
 {
 href: 'http://7xoqys.com1.z0.glb.clouddn.com/tumblr_oi65267haa1uygx22o1_500.jpg',
-type: '',
+type: 'image/jpeg',
 thumbnail: 'http://7xoqys.com1.z0.glb.clouddn.com/tumblr_oi65267haa1uygx22o1_500.jpg?imageView2/2/w/200'
 },
 {
 href: 'http://7xoqys.com1.z0.glb.clouddn.com/tumblr_oi66g7egHu1uygx22o1_500.jpg',
-type: '',
+type: 'image/jpeg',
 thumbnail: 'http://7xoqys.com1.z0.glb.clouddn.com/tumblr_oi66g7egHu1uygx22o1_500.jpg?imageView2/2/w/200'
 },
 {
 href: 'http://7xoqys.com1.z0.glb.clouddn.com/tumblr_oi66g7JQZz1uygx22o1_500.jpg',
-type: '',
+type: 'image/jpeg',
 thumbnail: 'http://7xoqys.com1.z0.glb.clouddn.com/tumblr_oi66g7JQZz1uygx22o1_500.jpg?imageView2/2/w/200'
 },
 {
 href: 'http://7xoqys.com1.z0.glb.clouddn.com/tumblr_oi5sk7O4yd1uygx22o1_500.jpg',
-type: '',
+type: 'image/jpeg',
 thumbnail: 'http://7xoqys.com1.z0.glb.clouddn.com/tumblr_oi5sk7O4yd1uygx22o1_500.jpg?imageView2/2/w/200'
 },
 {
 href: 'http://7xoqys.com1.z0.glb.clouddn.com/tumblr_oi5sk768to1uygx22o1_500.jpg',
-type: '',
+type: 'image/jpeg',
 thumbnail: 'http://7xoqys.com1.z0.glb.clouddn.com/tumblr_oi5sk768to1uygx22o1_500.jpg?imageView2/2/w/200'
 },
 {
 href: 'http://7xoqys.com1.z0.glb.clouddn.com/tumblr_oi5ty6628J1uygx22o1_500.jpg',
-type: '',
+type: 'image/jpeg',
 thumbnail: 'http://7xoqys.com1.z0.glb.clouddn.com/tumblr_oi5ty6628J1uygx22o1_500.jpg?imageView2/2/w/200'
 },
 {
 href: 'http://7xoqys.com1.z0.glb.clouddn.com/tumblr_oi5ty67Z9b1uygx22o1_500.jpg',
-type: '',
+type: 'image/jpeg',
 thumbnail: 'http://7xoqys.com1.z0.glb.clouddn.com/tumblr_oi5ty67Z9b1uygx22o1_500.jpg?imageView2/2/w/200'
 },
 {
 href: 'http://7xoqys.com1.z0.glb.clouddn.com/tumblr_oi5vc7iq101uygx22o1_500.jpg',
-type: '',
+type: 'image/jpeg',
 thumbnail: 'http://7xoqys.com1.z0.glb.clouddn.com/tumblr_oi5vc7iq101uygx22o1_500.jpg?imageView2/2/w/200'
 },
 {
 href: 'http://7xoqys.com1.z0.glb.clouddn.com/tumblr_oi5vc7PNmZ1uygx22o1_500.jpg',
-type: '',
+type: 'image/jpeg',
 thumbnail: 'http://7xoqys.com1.z0.glb.clouddn.com/tumblr_oi5vc7PNmZ1uygx22o1_500.jpg?imageView2/2/w/200'
 },
 {
 href: 'http://7xoqys.com1.z0.glb.clouddn.com/tumblr_oi5wq6UMjH1uygx22o1_500.jpg',
-type: '',
+type: 'image/jpeg',
 thumbnail: 'http://7xoqys.com1.z0.glb.clouddn.com/tumblr_oi5wq6UMjH1uygx22o1_500.jpg?imageView2/2/w/200'
 },
 {
 href: 'http://7xoqys.com1.z0.glb.clouddn.com/tumblr_oi5wq6tNdD1uygx22o1_500.jpg',
-type: '',
+type: 'image/jpeg',
 thumbnail: 'http://7xoqys.com1.z0.glb.clouddn.com/tumblr_oi5wq6tNdD1uygx22o1_500.jpg?imageView2/2/w/200'
 },
 {
 href: 'http://7xoqys.com1.z0.glb.clouddn.com/tumblr_oi5y47q9c31uygx22o1_500.jpg',
-type: '',
+type: 'image/jpeg',
 thumbnail: 'http://7xoqys.com1.z0.glb.clouddn.com/tumblr_oi5y47q9c31uygx22o1_500.jpg?imageView2/2/w/200'
 },
 {
 href: 'http://7xoqys.com1.z0.glb.clouddn.com/tumblr_oi5y47Zi1Z1uygx22o1_500.jpg',
-type: '',
+type: 'image/jpeg',
 thumbnail: 'http://7xoqys.com1.z0.glb.clouddn.com/tumblr_oi5y47Zi1Z1uygx22o1_500.jpg?imageView2/2/w/200'
 },
 {
 href: 'http://7xoqys.com1.z0.glb.clouddn.com/tumblr_oi44u6TFp21uygx22o1_500.jpg',
-type: '',
+type: 'image/jpeg',
 thumbnail: 'http://7xoqys.com1.z0.glb.clouddn.com/tumblr_oi44u6TFp21uygx22o1_500.jpg?imageView2/2/w/200'
 },
 {
 href: 'http://7xoqys.com1.z0.glb.clouddn.com/tumblr_oi44u6c0Rf1uygx22o1_500.jpg',
-type: '',
+type: 'image/jpeg',
 thumbnail: 'http://7xoqys.com1.z0.glb.clouddn.com/tumblr_oi44u6c0Rf1uygx22o1_500.jpg?imageView2/2/w/200'
 },
 {
 href: 'http://7xoqys.com1.z0.glb.clouddn.com/tumblr_oi4687Cjxk1uygx22o1_500.jpg',
-type: '',
+type: 'image/jpeg',
 thumbnail: 'http://7xoqys.com1.z0.glb.clouddn.com/tumblr_oi4687Cjxk1uygx22o1_500.jpg?imageView2/2/w/200'
 },
 {
 href: 'http://7xoqys.com1.z0.glb.clouddn.com/tumblr_oi4687Gzfd1uygx22o1_500.jpg',
-type: '',
+type: 'image/jpeg',
 thumbnail: 'http://7xoqys.com1.z0.glb.clouddn.com/tumblr_oi4687Gzfd1uygx22o1_500.jpg?imageView2/2/w/200'
 },
 {
 href: 'http://7xoqys.com1.z0.glb.clouddn.com/tumblr_oi47m61NQA1uygx22o1_500.png',
-type: '',
+type: 'image/png',
 thumbnail: 'http://7xoqys.com1.z0.glb.clouddn.com/tumblr_oi47m61NQA1uygx22o1_500.png?imageView2/2/w/200'
 },
 {
 href: 'http://7xoqys.com1.z0.glb.clouddn.com/tumblr_oi47m6IhO51uygx22o1_500.jpg',
-type: '',
+type: 'image/jpeg',
 thumbnail: 'http://7xoqys.com1.z0.glb.clouddn.com/tumblr_oi47m6IhO51uygx22o1_500.jpg?imageView2/2/w/200'
 },
 {
 href: 'http://7xoqys.com1.z0.glb.clouddn.com/tumblr_oi4907BXzL1uygx22o1_500.jpg',
-type: '',
+type: 'image/jpeg',
 thumbnail: 'http://7xoqys.com1.z0.glb.clouddn.com/tumblr_oi4907BXzL1uygx22o1_500.jpg?imageView2/2/w/200'
 },
 {
 href: 'http://7xoqys.com1.z0.glb.clouddn.com/tumblr_oi4907Vpcv1uygx22o1_500.jpg',
-type: '',
+type: 'image/jpeg',
 thumbnail: 'http://7xoqys.com1.z0.glb.clouddn.com/tumblr_oi4907Vpcv1uygx22o1_500.jpg?imageView2/2/w/200'
 },
 {
 href: 'http://7xoqys.com1.z0.glb.clouddn.com/tumblr_oi4ae6a3pH1uygx22o1_500.jpg',
-type: '',
+type: 'image/jpeg',
 thumbnail: 'http://7xoqys.com1.z0.glb.clouddn.com/tumblr_oi4ae6a3pH1uygx22o1_500.jpg?imageView2/2/w/200'
 },
 {
 href: 'http://7xoqys.com1.z0.glb.clouddn.com/tumblr_oi4ae6QhH41uygx22o1_500.jpg',
-type: '',
+type: 'image/jpeg',
 thumbnail: 'http://7xoqys.com1.z0.glb.clouddn.com/tumblr_oi4ae6QhH41uygx22o1_500.jpg?imageView2/2/w/200'
 },
 {
 href: 'http://7xoqys.com1.z0.glb.clouddn.com/tumblr_oi4bs6gvdb1uygx22o1_500.jpg',
-type: '',
+type: 'image/jpeg',
 thumbnail: 'http://7xoqys.com1.z0.glb.clouddn.com/tumblr_oi4bs6gvdb1uygx22o1_500.jpg?imageView2/2/w/200'
 },
 {
 href: 'http://7xoqys.com1.z0.glb.clouddn.com/tumblr_oi4bs7CRZk1uygx22o1_500.jpg',
-type: '',
+type: 'image/jpeg',
 thumbnail: 'http://7xoqys.com1.z0.glb.clouddn.com/tumblr_oi4bs7CRZk1uygx22o1_500.jpg?imageView2/2/w/200'
 },
 {
 href: 'http://7xoqys.com1.z0.glb.clouddn.com/tumblr_oi3xw7KLxR1uygx22o1_500.jpg',
-type: '',
+type: 'image/jpeg',
 thumbnail: 'http://7xoqys.com1.z0.glb.clouddn.com/tumblr_oi3xw7KLxR1uygx22o1_500.jpg?imageView2/2/w/200'
 },
 {
 href: 'http://7xoqys.com1.z0.glb.clouddn.com/tumblr_oi3xw7OCUR1uygx22o1_500.jpg',
-type: '',
+type: 'image/jpeg',
 thumbnail: 'http://7xoqys.com1.z0.glb.clouddn.com/tumblr_oi3xw7OCUR1uygx22o1_500.jpg?imageView2/2/w/200'
 },
 {
 href: 'http://7xoqys.com1.z0.glb.clouddn.com/tumblr_oi3za6E5sQ1uygx22o1_500.jpg',
-type: '',
+type: 'image/jpeg',
 thumbnail: 'http://7xoqys.com1.z0.glb.clouddn.com/tumblr_oi3za6E5sQ1uygx22o1_500.jpg?imageView2/2/w/200'
 },
 {
 href: 'http://7xoqys.com1.z0.glb.clouddn.com/tumblr_oi3za6VHSM1uygx22o1_500.jpg',
-type: '',
+type: 'image/jpeg',
 thumbnail: 'http://7xoqys.com1.z0.glb.clouddn.com/tumblr_oi3za6VHSM1uygx22o1_500.jpg?imageView2/2/w/200'
 },
 {
 href: 'http://7xoqys.com1.z0.glb.clouddn.com/tumblr_oi40o7IhzE1uygx22o1_500.jpg',
-type: '',
+type: 'image/jpeg',
 thumbnail: 'http://7xoqys.com1.z0.glb.clouddn.com/tumblr_oi40o7IhzE1uygx22o1_500.jpg?imageView2/2/w/200'
 },
 {
 href: 'http://7xoqys.com1.z0.glb.clouddn.com/tumblr_oi40o7h6u01uygx22o1_500.jpg',
-type: '',
+type: 'image/jpeg',
 thumbnail: 'http://7xoqys.com1.z0.glb.clouddn.com/tumblr_oi40o7h6u01uygx22o1_500.jpg?imageView2/2/w/200'
 },
 {
 href: 'http://7xoqys.com1.z0.glb.clouddn.com/tumblr_oi4226L21J1uygx22o1_500.jpg',
-type: '',
+type: 'image/jpeg',
 thumbnail: 'http://7xoqys.com1.z0.glb.clouddn.com/tumblr_oi4226L21J1uygx22o1_500.jpg?imageView2/2/w/200'
 },
 {
 href: 'http://7xoqys.com1.z0.glb.clouddn.com/tumblr_oi4226tkRb1uygx22o1_500.jpg',
-type: '',
+type: 'image/jpeg',
 thumbnail: 'http://7xoqys.com1.z0.glb.clouddn.com/tumblr_oi4226tkRb1uygx22o1_500.jpg?imageView2/2/w/200'
 },
 {
 href: 'http://7xoqys.com1.z0.glb.clouddn.com/tumblr_oi43g7yDKg1uygx22o1_500.jpg',
-type: '',
+type: 'image/jpeg',
 thumbnail: 'http://7xoqys.com1.z0.glb.clouddn.com/tumblr_oi43g7yDKg1uygx22o1_500.jpg?imageView2/2/w/200'
 },
 {
 href: 'http://7xoqys.com1.z0.glb.clouddn.com/tumblr_oi43g7IgmL1uygx22o1_500.jpg',
-type: '',
+type: 'image/jpeg',
 thumbnail: 'http://7xoqys.com1.z0.glb.clouddn.com/tumblr_oi43g7IgmL1uygx22o1_500.jpg?imageView2/2/w/200'
 },
 {
 href: 'http://7xoqys.com1.z0.glb.clouddn.com/tumblr_oi2607LtJX1uygx22o1_500.jpg',
-type: '',
+type: 'image/jpeg',
 thumbnail: 'http://7xoqys.com1.z0.glb.clouddn.com/tumblr_oi2607LtJX1uygx22o1_500.jpg?imageView2/2/w/200'
 },
 {
 href: 'http://7xoqys.com1.z0.glb.clouddn.com/tumblr_oi27e7XNiP1uygx22o1_500.jpg',
-type: '',
+type: 'image/jpeg',
 thumbnail: 'http://7xoqys.com1.z0.glb.clouddn.com/tumblr_oi27e7XNiP1uygx22o1_500.jpg?imageView2/2/w/200'
 },
 {
 href: 'http://7xoqys.com1.z0.glb.clouddn.com/tumblr_oi27e7WMgr1uygx22o1_500.jpg',
-type: '',
+type: 'image/jpeg',
 thumbnail: 'http://7xoqys.com1.z0.glb.clouddn.com/tumblr_oi27e7WMgr1uygx22o1_500.jpg?imageView2/2/w/200'
 },
 {
 href: 'http://7xoqys.com1.z0.glb.clouddn.com/tumblr_oi28s6wV0Q1uygx22o1_500.jpg',
-type: '',
+type: 'image/jpeg',
 thumbnail: 'http://7xoqys.com1.z0.glb.clouddn.com/tumblr_oi28s6wV0Q1uygx22o1_500.jpg?imageView2/2/w/200'
 },
 {
 href: 'http://7xoqys.com1.z0.glb.clouddn.com/tumblr_oi28s6WXSK1uygx22o1_500.png',
-type: '',
+type: 'image/png',
 thumbnail: 'http://7xoqys.com1.z0.glb.clouddn.com/tumblr_oi28s6WXSK1uygx22o1_500.png?imageView2/2/w/200'
 },
 {
 href: 'http://7xoqys.com1.z0.glb.clouddn.com/tumblr_oi2a66GX7f1uygx22o1_500.jpg',
-type: '',
+type: 'image/jpeg',
 thumbnail: 'http://7xoqys.com1.z0.glb.clouddn.com/tumblr_oi2a66GX7f1uygx22o1_500.jpg?imageView2/2/w/200'
 },
 {
 href: 'http://7xoqys.com1.z0.glb.clouddn.com/tumblr_oi2a66w8EP1uygx22o1_500.jpg',
-type: '',
+type: 'image/jpeg',
 thumbnail: 'http://7xoqys.com1.z0.glb.clouddn.com/tumblr_oi2a66w8EP1uygx22o1_500.jpg?imageView2/2/w/200'
 },
 {
 href: 'http://7xoqys.com1.z0.glb.clouddn.com/tumblr_oi2bk7VIBg1uygx22o1_500.jpg',
-type: '',
+type: 'image/jpeg',
 thumbnail: 'http://7xoqys.com1.z0.glb.clouddn.com/tumblr_oi2bk7VIBg1uygx22o1_500.jpg?imageView2/2/w/200'
 },
 {
 href: 'http://7xoqys.com1.z0.glb.clouddn.com/tumblr_oi2bk7q8ea1uygx22o1_500.jpg',
-type: '',
+type: 'image/jpeg',
 thumbnail: 'http://7xoqys.com1.z0.glb.clouddn.com/tumblr_oi2bk7q8ea1uygx22o1_500.jpg?imageView2/2/w/200'
 },
 {
 href: 'http://7xoqys.com1.z0.glb.clouddn.com/tumblr_oi2cy63jcW1uygx22o1_500.jpg',
-type: '',
+type: 'image/jpeg',
 thumbnail: 'http://7xoqys.com1.z0.glb.clouddn.com/tumblr_oi2cy63jcW1uygx22o1_500.jpg?imageView2/2/w/200'
 },
 {
 href: 'http://7xoqys.com1.z0.glb.clouddn.com/tumblr_oi2cy67pwZ1uygx22o1_500.png',
-type: '',
+type: 'image/png',
 thumbnail: 'http://7xoqys.com1.z0.glb.clouddn.com/tumblr_oi2cy67pwZ1uygx22o1_500.png?imageView2/2/w/200'
 },
 {
 href: 'http://7xoqys.com1.z0.glb.clouddn.com/tumblr_oi2ec6Ws4u1uygx22o1_500.jpg',
-type: '',
+type: 'image/jpeg',
 thumbnail: 'http://7xoqys.com1.z0.glb.clouddn.com/tumblr_oi2ec6Ws4u1uygx22o1_500.jpg?imageView2/2/w/200'
 },
 {
 href: 'http://7xoqys.com1.z0.glb.clouddn.com/tumblr_oi2ec6PoxX1uygx22o1_500.jpg',
-type: '',
+type: 'image/jpeg',
 thumbnail: 'http://7xoqys.com1.z0.glb.clouddn.com/tumblr_oi2ec6PoxX1uygx22o1_500.jpg?imageView2/2/w/200'
 },
 {
 href: 'http://7xoqys.com1.z0.glb.clouddn.com/tumblr_oi2fq7hJE61uygx22o1_500.jpg',
-type: '',
+type: 'image/jpeg',
 thumbnail: 'http://7xoqys.com1.z0.glb.clouddn.com/tumblr_oi2fq7hJE61uygx22o1_500.jpg?imageView2/2/w/200'
 },
 {
 href: 'http://7xoqys.com1.z0.glb.clouddn.com/tumblr_oi2fq79QV71uygx22o1_500.jpg',
-type: '',
+type: 'image/jpeg',
 thumbnail: 'http://7xoqys.com1.z0.glb.clouddn.com/tumblr_oi2fq79QV71uygx22o1_500.jpg?imageView2/2/w/200'
 },
 {
 href: 'http://7xoqys.com1.z0.glb.clouddn.com/tumblr_oi2h46YGCS1uygx22o1_500.jpg',
-type: '',
+type: 'image/jpeg',
 thumbnail: 'http://7xoqys.com1.z0.glb.clouddn.com/tumblr_oi2h46YGCS1uygx22o1_500.jpg?imageView2/2/w/200'
 },
 {
 href: 'http://7xoqys.com1.z0.glb.clouddn.com/tumblr_oi2h46AdrW1uygx22o1_400.jpg',
-type: '',
+type: 'image/jpeg',
 thumbnail: 'http://7xoqys.com1.z0.glb.clouddn.com/tumblr_oi2h46AdrW1uygx22o1_400.jpg?imageView2/2/w/200'
 },
 {
 href: 'http://7xoqys.com1.z0.glb.clouddn.com/tumblr_oi2ii7gN7w1uygx22o1_500.jpg',
-type: '',
+type: 'image/jpeg',
 thumbnail: 'http://7xoqys.com1.z0.glb.clouddn.com/tumblr_oi2ii7gN7w1uygx22o1_500.jpg?imageView2/2/w/200'
 },
 {
 href: 'http://7xoqys.com1.z0.glb.clouddn.com/tumblr_oi2ii71iEk1uygx22o1_500.jpg',
-type: '',
+type: 'image/jpeg',
 thumbnail: 'http://7xoqys.com1.z0.glb.clouddn.com/tumblr_oi2ii71iEk1uygx22o1_500.jpg?imageView2/2/w/200'
 },
 {
 href: 'http://7xoqys.com1.z0.glb.clouddn.com/tumblr_oi2607ATnA1uygx22o1_500.jpg',
-type: '',
+type: 'image/jpeg',
 thumbnail: 'http://7xoqys.com1.z0.glb.clouddn.com/tumblr_oi2607ATnA1uygx22o1_500.jpg?imageView2/2/w/200'
 }
 ],{
