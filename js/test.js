b5aaec123c9ff2ed6efb70d242b2fe5dee111b9d
roadli <roadli@gmail.com>
2016-12-17 10:09:02 +0800

    Daily update images file @2016-12-17

--- js/test.js
+++ js/test.js
@@ -1,5 +1,15 @@
   blueimp.Gallery([
 {
+href: 'http://7xoqys.com1.z0.glb.clouddn.com/tumblr_oi0bc3TaSf1qfi26mo1_500.jpg',
+type: 'image/jpeg',
+thumbnail: 'http://7xoqys.com1.z0.glb.clouddn.com/tumblr_oi0bc3TaSf1qfi26mo1_500.jpg?imageView2/2/w/200'
+},
+{
+href: 'http://7xoqys.com1.z0.glb.clouddn.com/tumblr_oi05s4DBGq1qfi26mo1_500.jpg',
+type: 'image/jpeg',
+thumbnail: 'http://7xoqys.com1.z0.glb.clouddn.com/tumblr_oi05s4DBGq1qfi26mo1_500.jpg?imageView2/2/w/200'
+},
+{
 href: 'http://7xoqys.com1.z0.glb.clouddn.com/tumblr_oi9ou6ANKu1uygx22o1_500.jpg',
 type: 'image/jpeg',
 thumbnail: 'http://7xoqys.com1.z0.glb.clouddn.com/tumblr_oi9ou6ANKu1uygx22o1_500.jpg?imageView2/2/w/200'
